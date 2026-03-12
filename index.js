58f5c4e98d8d1197253d1f5cbb6da3926955b604
chopo <elchopo7@gmail.com>
2026-03-12 17:19:53 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+let count = 0
+let countTwo = 0
+
+let num1 = document.querySelector("#num1-el")
+let num2 = document.querySelector("#num2-el")
+
+function add() {
+    count += 1
+    let scoreFree = count
+    num1.textContent = scoreFree
+    console.log("Home Team has "+ count)   
+}
+
+
+
+
+
+
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
